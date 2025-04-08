98caa27d67350b494607a86c2aa1700067b725a4
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-04-08 21:36:01 +0300

    create About Component

--- src/components/App.js
+++ src/components/App.js
@@ -16,14 +16,24 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About(){
+  return (
+    <div id="about">
+      <h2>About</h2>
+
+    </div>
+
+  )
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
+      {<Home />}
+      {/* add your <About> component here */
+      <About />}
       {/* add your <About> component here */}
     </div>
   );
